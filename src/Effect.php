52fb9a81ff4e24a3e8e9598bcd19f42e946ffe7b
annieversary <annie@versary.town>
2024-06-17 22:25:31 +0100

    Handle one at a time, yield effects from a handler

--- src/Effect.php
+++ src/Effect.php
@@ -2,7 +2,7 @@
 
 namespace Annieversary\EffectSystem;
 
-function handle(\Generator $gen, array|Handler $handlers) {
+function run(\Generator $gen, array|Handler|null $handlers = null) {
     if ($handlers instanceof Handler) {
         $handlers = [$handlers];
     }
@@ -10,22 +10,59 @@ function handle(\Generator $gen, array|Handler $handlers) {
     $handle = function (Effect $effect) use ($handlers) {
         foreach ($handlers as $handler) {
             if ($effect instanceof $handler::$effect) {
-                return $handler->handle($effect);
+                $o = $handler->handle($effect);
+
+                if ($o instanceof \Generator) {
+                    return run($o, $handlers);
+                }
+
+                return $o;
             }
         }
 
         // TODO change to new class
-        throw new \Exception('Missing Handler for Effect ' . $effect::class);
+        throw new \Exception('Unhandled effect ' . $effect::class);
     };
 
     while (true) {
-        $effect = $gen->current();
-        $output = $handle($effect);
-        $gen->send($output);
-
         if (!$gen->valid()) {
             return $gen->getReturn();
         }
+
+        $effect = $gen->current();
+        $output = $handle($effect);
+        $gen->send($output);
+    }
+}
+
+function handle(\Generator $gen, array|Handler $handlers) {
+    if ($handlers instanceof Handler) {
+        $handlers = [$handlers];
+    }
+
+    while (true) {
+        if (!$gen->valid()) {
+            return $gen->getReturn();
+        }
+
+        $effect = $gen->current();
+
+        $output = [];
+        foreach ($handlers as $handler) {
+            if ($effect instanceof $handler::$effect) {
+                $o = $handler->handle($effect);
+                if ($o instanceof \Generator) {
+                    $output[] = yield from $o;
+                } else {
+                    $output[] = $o;
+                }
+            }
+        }
+        if (count($output) == 0) {
+            $output[] = yield $effect;
+        }
+
+        $gen->send($output[0]);
     }
 }
 
--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -2,7 +2,9 @@
 
 namespace Annieversary\EffectSystem;
 
-class BasicTest extends \PHPUnit\Framework\TestCase
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
 {
     function program() {
         $v = yield new AddNumbers(3, 7);
@@ -14,7 +16,48 @@ class BasicTest extends \PHPUnit\Framework\TestCase
     public function test_works() {
         $gen = $this->program();
 
-        $result = handle($gen, [new AddNumberHandler, new SubNumberHandler]);
+        $result = run($gen, [new AddNumberHandler, new SubNumberHandler]);
+
+        $this->assertEquals(8, $result);
+    }
+
+    public function test_program() {
+        $gen = $this->program();
+
+        $this->assertInstanceOf(AddNumbers::class, $gen->current());
+        $gen->send(10);
+        $this->assertInstanceOf(SubNumbers::class, $gen->current());
+        $gen->send(10);
+        $this->assertFalse($gen->valid());
+        $this->assertEquals(10, $gen->getReturn());
+    }
+
+    public function test_half_handle() {
+        $gen = $this->program();
+
+        $gen = handle($gen, new AddNumberHandler);
+
+        $this->assertInstanceOf(SubNumbers::class, $gen->current());
+        $gen->send(10);
+        $this->assertFalse($gen->valid());
+        $this->assertEquals(10, $gen->getReturn());
+    }
+
+    public function test_handle_and_run() {
+        $gen = $this->program();
+
+        $adds_handled = handle($gen, new AddNumberHandler);
+        $result = run($adds_handled, new SubNumberHandler);
+
+        $this->assertEquals(8, $result);
+    }
+
+    public function test_both_handled() {
+        $gen = $this->program();
+
+        $adds_handled = handle($gen, new AddNumberHandler);
+        $subs_handled = handle($adds_handled, new SubNumberHandler);
+        $result = run($subs_handled);
 
         $this->assertEquals(8, $result);
     }
@@ -22,7 +65,7 @@ class BasicTest extends \PHPUnit\Framework\TestCase
     public function test_handlers_in_inverse_order_works() {
         $gen = $this->program();
 
-        $result = handle($gen, [new SubNumberHandler, new AddNumberHandler]);
+        $result = run($gen, [new SubNumberHandler, new AddNumberHandler]);
 
         $this->assertEquals(8, $result);
     }
@@ -32,7 +75,25 @@ class BasicTest extends \PHPUnit\Framework\TestCase
 
         $this->expectException(\Exception::class);
 
-        handle($gen, [new AddNumberHandler]);
+        run($gen, [new AddNumberHandler]);
+    }
+
+    public function test_handler_that_yields() {
+        $gen = $this->program();
+
+        $result = run($gen, [new SubNumberByAddHandler, new AddNumberHandler]);
+
+        $this->assertEquals(8, $result);
+    }
+
+    public function test_handle_handler_that_yields() {
+        $gen = $this->program();
+
+        $adds_handled = handle($gen, new AddNumberHandler);
+        $subs_handled = handle($adds_handled, new SubNumberHandler);
+        $result = run($subs_handled);
+
+        $this->assertEquals(8, $result);
     }
 }
 
@@ -57,5 +118,13 @@ class SubNumberHandler extends Handler {
 
     public function handle(mixed $effect) {
         return $effect->a - $effect->b;
+    }
+}
+
+class SubNumberByAddHandler extends Handler {
+    public static $effect = SubNumbers::class;
+
+    public function handle(mixed $effect) {
+        return yield new AddNumbers($effect->a, -$effect->b);
     }
 }
